2d078a6a71d2300e2b349a414d7d0a2b064a3213
Anukriti-Mishra <anukritimishra16@gmail.com>
2024-03-04 19:20:21 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var tl=gsap.timeline()
+{
+    tl.from("#page1",
+    {
+        y:"90vh",
+        duration:1.5,
+        delay:0.7 
+        
+    })
+    tl.to("#page1",
+    {
+        y:"0vh",
+    })
+}
+
